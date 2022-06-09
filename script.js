bc22138c7ec6056ef0fc4e081d03a71f87af08ea
cinzia10 <103512084+cinzia10@users.noreply.github.com>
2022-06-09 22:40:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let array = [];
 //// FUNCTION CHE CONTROLLA L'ID DELLA PAGINA
 function parsUrlParams() {
     const urlSerchParams = new URLSearchParams(window.location.search)
-     const param = Object.fromEntries(urlSerchParams)
-     return param;      
+    const param = Object.fromEntries(urlSerchParams)
+    return param;
 }
 
 
@@ -21,61 +21,26 @@ function displayObject(obj) {
 
 function display(array) {
     console.log(array);
-    const pg = document.getElementById('container-character');
+    const container = document.getElementById('container-character');
 
 
-    const title = document.createElement('h1')
-    const textTitle = document.createTextNode(array.name)
-    title.appendChild(textTitle)
-    pg.appendChild(title)
-    const img = document.createElement('img')
-       img.src = './assets/' + array.index + ".png" 
-       img.classList.add('info-img')
-    pg.appendChild(img)
+    const title = document.createElement('h1');
+    const textTitle = document.createTextNode('Name: ' + array.name);
+    title.appendChild(textTitle);
 
-    const list = document.createElement('ul')
-    list.classList.add('list')
-    const size = document.createElement('p')
-    const nodesize = document.createTextNode('Size: ' + array.size + ', ' + array.size_description)
-    size.classList.add('size-list')
-    const traits = document.createElement('p')
-    const nodeatraits = document.createTextNode('Traits: ' + array.traits)
-    traits.classList.add('traits-list')
-    const language = document.createElement('p')
-    const nodealanguage = document.createTextNode('Languages: ' + array.language_desc)
-    language.classList.add('language-list')
-    size.appendChild(nodesize)
-    traits.appendChild(nodeatraits)
-    language.appendChild(nodealanguage)
-    list.appendChild(nodesize)
-    list.appendChild(nodeatraits)
-    list.appendChild(nodealanguage)
-    pg.appendChild(size)
-    pg.appendChild(traits)
-    pg.appendChild(language)
-    pg.appendChild(list)
+    const img = document.createElement('img');
+    img.src = './assets/' + array.index + ".png";
+    img.classList.add('info-img');
 
-    const alignment = document.createElement('p')
-    const nodealignment = document.createTextNode(array.alignment)
-    alignment.appendChild(nodealignment)
-    pg.appendChild(alignment)
-    
-
-    const age = document.createElement('p')
-    const textage = document.createTextNode(array.age)
-    age.appendChild(textage)
-    pg.appendChild(age)
-
- 
-
+    container.append(title, img)
 }
 
 
 
 function requestData() {
     fetch(BASE_URL)
-    .then(response => response.json())
-    .then(result => displayObject(result))
+        .then(response => response.json())
+        .then(result => displayObject(result))
 }
 
 
